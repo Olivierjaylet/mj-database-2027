10b3040bf0f54515832372e878898011e399a131
Ipuch <pierre.puchaud@umontreal.ca>
2024-12-15 17:12:37 -0500

    fine tuning
    fix
    
    delete

--- merge.py
+++ merge.py
@@ -1,29 +1,33 @@
 import pandas as pd
 
-POPULATION = "all"
-POLLS_FILE = 'polls.csv'
-POLL_TYPES_FILE = 'poll_types.csv'
-CANDIDATES_FILE = 'candidates.csv'
-OUTPUT_FILE = "mj2027.csv"
+POLLS_FILE = "polls.csv"
+POLL_TYPES_FILE = "poll_types.csv"
+CANDIDATES_FILE = "candidates.csv"
+
 
 def load_data(file_path):
     return pd.read_csv(file_path)
 
+
 def get_poll_file_path(poll, population):
     return f"{poll['folder']}/{poll['poll_id']}_{population}.csv"
+
 
 def add_poll_metadata(poll_df, poll):
     for col, value in poll.items():
         poll_df[col] = value
     return poll_df
 
+
 def add_poll_type_metadata(poll_df, poll_type):
     for col, value in poll_type.items():
         poll_df[col] = value
     return poll_df
 
+
 def merge_candidate_metadata(poll_df, candidates):
     return poll_df.merge(candidates, on="candidate_id", how="left")
+
 
 def process_polls(polls, poll_types, candidates, population):
     merged_df = pd.DataFrame()
@@ -34,7 +38,7 @@ def process_polls(polls, poll_types, candidates, population):
         poll_type_id = poll_df["poll_type_id"].unique()[0]
 
         poll_df = add_poll_metadata(poll_df, poll)
-        poll_type = poll_types[poll_types['id'] == poll_type_id].iloc[0]
+        poll_type = poll_types[poll_types["id"] == poll_type_id].iloc[0]
         poll_df = add_poll_type_metadata(poll_df, poll_type)
         poll_df = merge_candidate_metadata(poll_df, candidates)
 
@@ -42,16 +46,25 @@ def process_polls(polls, poll_types, candidates, population):
 
     return merged_df
 
+
 def main():
-    polls = load_data(POLLS_FILE)
-    poll_types = load_data(POLL_TYPES_FILE)
-    candidates = load_data(CANDIDATES_FILE)
 
-    print(polls)
+    POPULATIONS = ["all", "left", "farright", "macron", "absentionists"]
 
-    merged_df = process_polls(polls, poll_types, candidates, POPULATION)
-    merged_df.to_csv(OUTPUT_FILE, index=False)
+    for POPULATION in POPULATIONS:
+        OUTPUT_FILE = f"mj2027_{POPULATION}.csv" if POPULATION != "all" else "mj2027.csv"
+
+        polls = load_data(POLLS_FILE)
+        polls = polls[polls["population"] == POPULATION]
+
+        poll_types = load_data(POLL_TYPES_FILE)
+        candidates = load_data(CANDIDATES_FILE)
+
+        print(polls)
+
+        merged_df = process_polls(polls, poll_types, candidates, POPULATION)
+        merged_df.to_csv(OUTPUT_FILE, index=False)
+
 
 if __name__ == "__main__":
     main()
-
