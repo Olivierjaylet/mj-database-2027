a9129e9b32fb1d4acbfc67d8930273ff9d202fcb
ipuch <pierre.puchaud@inria.fr>
2025-10-14 16:37:06 +0200

    linting

--- tests/test_poll_format.py
+++ tests/test_poll_format.py
@@ -1,10 +1,11 @@
 import os
 import pandas as pd
 
+
 def test_poll_filename_format():
 
-    for item in os.listdir('polls/'):
-        item_path = os.path.join('polls/', item)
+    for item in os.listdir("polls/"):
+        item_path = os.path.join("polls/", item)
 
         if os.path.isdir(item_path):
 
@@ -13,25 +14,24 @@ def test_poll_filename_format():
 
                 if os.path.isfile(file_path):
 
-                    assert '_' in file, f"Nom de fichier invalide dans {item}/ : {file}"
+                    assert "_" in file, f"Nom de fichier invalide dans {item}/ : {file}"
 
-                    assert file.endswith('.csv'), f"Extension invalide dans {item}/ : {file}"
+                    assert file.endswith(".csv"), f"Extension invalide dans {item}/ : {file}"
 
 
 def test_poll_references_valid_candidates():
 
-    candidates = pd.read_csv('candidates.csv')['candidate_id'].tolist()
+    candidates = pd.read_csv("candidates.csv")["candidate_id"].tolist()
 
-    for item in os.listdir('polls/'):
-        item_path = os.path.join('polls/', item)
+    for item in os.listdir("polls/"):
+        item_path = os.path.join("polls/", item)
 
         if os.path.isdir(item_path):
             for file in os.listdir(item_path):
                 file_path = os.path.join(item_path, file)
 
-                if os.path.isfile(file_path) and file.endswith('.csv'):
+                if os.path.isfile(file_path) and file.endswith(".csv"):
                     df = pd.read_csv(file_path)
 
-                    for candidate_ref in df['candidate_id']:  
-                        assert candidate_ref in candidates, \
-                            f"Candidat invalide '{candidate_ref}' dans {file_path}"
+                    for candidate_ref in df["candidate_id"]:
+                        assert candidate_ref in candidates, f"Candidat invalide '{candidate_ref}' dans {file_path}"
